d812bfcc904eebe85dff9bbdcd641f0c03cdbdd4
Denys Ivchenko <56501591+denys-ivchenko@users.noreply.github.com>
2021-12-07 08:12:40 +0200

    Доведення проектыв до респектабельного вигляду

--- SecuredSource/Properties/AssemblyInfo.cs
+++ SecuredSource/Properties/AssemblyInfo.cs
@@ -8,18 +8,18 @@ using System.Windows;
 // набор атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
 [assembly: AssemblyTitle("Secured Source")]
-[assembly: AssemblyDescription("Secured Source is encryption software.")]
+[assembly: AssemblyDescription("Cryptographic file encryption software")]
 [assembly: AssemblyConfiguration("Debug")]
 [assembly: AssemblyCompany("Telesyk")]
 [assembly: AssemblyProduct("Secured Source")]
 [assembly: AssemblyCopyright("Copyright © Telesyk 2021")]
-[assembly: AssemblyTrademark("Secured Source")]
+[assembly: AssemblyTrademark("Telesyk Cryptographic Software")]
 [assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("1.0.1.*")]
+[assembly: AssemblyFileVersion("1.0.1.21")]
 
-// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
-// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
-// из модели COM, установите атрибут ComVisible для этого типа в значение true.
 [assembly: ComVisible(false)]
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
 
 //Чтобы начать создание локализуемых приложений, задайте
 //<UICulture>CultureYouAreCodingWith</UICulture> в файле .csproj
@@ -29,27 +29,3 @@ using System.Windows;
 //строка внизу для обеспечения соответствия настройки UICulture в файле проекта.
 
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
-
-
-[assembly: ThemeInfo(
-	ResourceDictionaryLocation.None, //где расположены словари ресурсов по конкретным тематикам
-									 //(используется, если ресурс не найден на странице,
-									 // или в словарях ресурсов приложения)
-	ResourceDictionaryLocation.SourceAssembly //где расположен словарь универсальных ресурсов
-											  //(используется, если ресурс не найден на странице,
-											  // в приложении или в каких-либо словарях ресурсов для конкретной темы)
-)]
-
-
-// Сведения о версии для сборки включают четыре следующих значения:
-//
-//      Основной номер версии
-//      Дополнительный номер версии
-//      Номер сборки
-//      Номер редакции
-//
-// Можно задать все значения или принять номера сборки и редакции по умолчанию 
-// используя "*", как показано ниже:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1.*")]
-[assembly: AssemblyFileVersion("1.0.1.17")]
